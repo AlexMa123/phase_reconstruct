9a92fd882f0975c69f415a55eb02c8b360c70f01
Yaopeng-PC <mailmyps@gmail.com>
2020-10-23 14:22:26 +0300

    change the docstring style from google to numpy

--- tools.py
+++ tools.py
@@ -1,19 +1,57 @@
+from typing import Tuple
 import numpy as np
 from numba import njit  # jit
 import mkl_fft
 import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+
+@njit(cache=True)
+def rotate(x: np.ndarray, y: np.ndarray,
+           theta: float) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    perform a rotation in Euclidean space
+
+    Parameters
+    ----------
+        x : np.ndarray
+            x
+
+        y : np.ndarray
+            y
+
+        theta : float
+            angle
+
+    Returns
+    -------
+        Tuple[np.ndarray, np.ndarray]
+            new (x, y)
+    """
+    new_x = np.cos(theta) * x - np.sin(theta) * y
+    new_y = np.sin(theta) * x + np.cos(theta) * y
+    return new_x, new_y
 
 
 @njit(cache=True)
 def get_length(x: np.ndarray, y: np.ndarray) -> np.ndarray:
-    """calculate the length of the trajectory in phase space
+    """
+    get_length(x, y)
 
-    Args:
-        x (np.ndarray): x(t)
-        y (np.ndarray): y(t)
+    calculate the length of the trajectory in phase space
 
-    Returns:
-        np.ndarray: the length L(t)
+    Parameters
+    ----------
+        x : np.ndarray
+            x(t)
+
+        y : np.ndarray
+            y(t)
+
+    Returns
+    -------
+        np.ndarray
+            the length L(t)
     """
     diff_x2 = np.zeros_like(x)
     diff_y2 = np.zeros_like(y)
@@ -24,23 +62,39 @@ def get_length(x: np.ndarray, y: np.ndarray) -> np.ndarray:
 
 
 def hilbert(signal: np.ndarray) -> np.ndarray:
-    """calculate the analytic signal using the Hilbert transform (same with scipy.signal.hilbert)
+    """
+    hilbert(signal)
 
-    Args:
-        signal ([np.ndarray]): signal
+    calculate the analytic signal using the `Hilbert transform` (same with scipy.signal.hilbert)
 
-    Returns:
-        [np.ndarray]: s + s_H * i
+    .. math::
+        S_a = S + iS_H
+
+        where S_H is the hilbert transform of the signal
+
+    Parameters
+    ----------
+        signal : np.ndarray
+            signal
+
+    Returns
+    -------
+        np.ndarray
+           :math:'s + s_{H} i'
     """
     @njit("c16[:](c16[:])")
     def _modifyfft(x: np.ndarray) -> np.ndarray:
         """used by the function hilbert
 
-        Args:
-            x (np.ndarray): [description]
+        Parameters
+        ----------
+            x : np.ndarray
+                [description]
 
-        Returns:
-            np.ndarray: [description]
+        Returns
+        -------
+            np.ndarray
+                [description]
         """
         N = x.size
         if N % 2 == 0:
@@ -59,14 +113,21 @@ def hilbert(signal: np.ndarray) -> np.ndarray:
 
 def get_protophase(signal: np.ndarray,
                    event_markers: np.ndarray) -> np.ndarray:
-    """calculate protophase from signal based on the general event marker method (Eq 8)
+    """
+    calculate protophase from signal based on the `general event marker method` (Eq 8)
 
-    Args:
-        signal (np.ndarray): real signal
-        event_markers (np.ndarray[int]): where(index of the signal array) the trajectory go through the Poincare section
+    Parameters
+    ----------
+        signal : np.ndarray
+            real signal
 
-    Returns:
-        np.ndarray: protophase (without mod 2pi)
+        event_markers : np.ndarray
+            where(index of the signal array) the trajectory go through the Poincare section
+
+    Returns
+    -------
+        np.ndarray
+            protophase (without mod 2\\pi)
     """
     signal = signal - np.mean(signal)
     s_a = hilbert(signal)
@@ -103,13 +164,60 @@ def get_protophase(signal: np.ndarray,
         return _cal_protophase(x, y, event_markers)
 
 
+def get_protophase_hilbert(signal: np.ndarray,
+                           y0: float = 0,
+                           y0_hat: float = 0) -> np.ndarray:
+    """
+    calculate protophase from signal based on the hilbert plane phase(Eq 7)
+
+    Parameters
+    ----------
+        signal : np.ndarray
+            real signal
+
+        y0 : float
+            y0
+
+        y0_hat : float
+            y0_hat
+    Returns
+    -------
+        np.ndarray
+    """
+    s_a = hilbert(signal)
+    y = s_a.real - y0
+    y_hat = s_a.imag - y0_hat
+    protophase = np.arctan2(y_hat, y)
+    return protophase
+
+
 if __name__ == "__main__":
-    f = 10
-    x = np.linspace(0, 8 * np.pi, 1000 * f)
-    y = np.sin(x) + np.random.rand(1000 * f) * 1
-    w0 = 1
-    event_markers = np.array([250 * f, 500 * f, 750 * f])
+
+    def van_der_pol(t, x_v, mu, omega):
+        x, y = x_v
+        dxdt = y
+        dydt = 0.05 * np.random.randn() + mu * (1 - x**2) * y - omega**2 * x
+        return np.array([dxdt, dydt])
+
+    [mu, omega] = 0.5, 1.11
+    sol = solve_ivp(van_der_pol, [0, 100], [2, 0],
+                    args=[mu, omega],
+                    max_step=0.1)
+    x = sol.y[0]
+    signal = (x**2 - 1.7) * x
+    s_a = hilbert(signal)
+    y = s_a.real
+    y_hat = s_a.imag
+    new_y, new_yhat = rotate(y, y_hat, (np.pi / 2))
+    phase = np.arctan2(new_yhat, new_y)
     plt.figure()
-    plt.hist(get_protophase(y, event_markers) - w0 * x, bins=100)
-    # plt.ylim([-0.3, 0.3])
+    plt.plot(y, y_hat)
+    plt.figure()
+    plt.plot(new_y, new_yhat)
+    plt.figure()
+    plt.plot(sol.t, phase)
     plt.show()
+    # phase = get_protophase_hilbert(x, 0, 0.8)
+    # plt.figure()
+    # plt.plot(sol.t, phase)
+    # plt.show()
